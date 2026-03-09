24157a17530227684ca64a2c039d36df2ca10c89
ANZZCUKKK <ntr970048@gmail.com>
2026-03-10 02:02:21 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  BOT_TOKEN: "8755785093:AAENxDNgqakLe84hx3pHrSOd8LCbk_MhXMM",
-  OWNER_ID: 6484697408,
-  GITHUB_TOKEN: "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",
-  GITHUB_USER: "ANZZCUKKK",
-  GITHUB_REPO: "telegram-key-bot",
-  KEY_PREFIX: "ANZZ",
-  START_PHOTO: "https://files.catbox.moe/amjkjy.jpg"
-};
