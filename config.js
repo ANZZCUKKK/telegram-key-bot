1b3bdd8c941cf283237f0c37842e1b99be79d7ec
ANZZCUKKK <ntr970048@gmail.com>
2026-03-09 23:40:39 +0700

    Create config.js with bot configuration
    Add configuration settings for bot tokens and IDs.

---
+++ config.js
@@ -0,0 +1,15 @@
+export default {
+
+BOT_TOKEN: "8755785093:AAENxDNgqakLe84hx3pHrSOd8LCbk_MhXMM",
+
+OWNER_ID: 6484697408,
+
+GITHUB_TOKEN: "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",
+
+GITHUB_USER: "ANZZCUKKK",
+
+GITHUB_REPO: "telegram-key-bot",
+
+KEY_PREFIX: "ANZZ"
+
+}
